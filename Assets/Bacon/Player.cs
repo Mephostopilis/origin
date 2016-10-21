7c9487e41fe4aaf1b86a57a49c38b11cda3642f8
emberfarkas <hubing91@163.com>
2016-10-21 20:20:35 +0800

    fix

---
+++ Assets/Bacon/Player.cs
@@ -0,0 +1,51 @@
+﻿using Maria.Network;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Bacon {
+    class Player {
+
+        protected uint _session = 0;
+        private Dictionary<long, MyBall> _myballs = new Dictionary<long, MyBall>();
+
+        public Player() {
+        }
+
+        public uint Session { get { return _session; } set { _session = value; } }
+
+        public void Add(MyBall ball) {
+            _myballs[ball.Id] = ball;
+        }
+
+        public void Remove(MyBall ball) {
+            _myballs.Remove(ball.Id);
+        }
+
+        public void Clear() {
+            _myballs = new Dictionary<long, MyBall>();
+        }
+
+
+        public byte[] PackBall() {
+            int idx = 0;
+            byte[] buf = new byte[32 * _myballs.Count];
+            foreach (var ball in _myballs) {
+                NetPack.Packll(buf, 32 * idx + 0, ball.Value.Id);
+                byte[] pos = ball.Value.PackPos();
+                byte[] dir = ball.Value.PackDir();
+                Array.Copy(pos, 0, buf, 28 * idx + 8, pos.Length);
+                Array.Copy(dir, 0, buf, 28 * idx + 20, dir.Length);
+                idx++;
+            }
+            return buf;
+        }
+
+        public void ChangeDir(Vector3 dir) {
+            for (int i = 0; i < _myballs.Count; i++) {
+                var ball = _myballs[i];
+                ball.Dir = dir;
+            }
+        }
+    }
+}
