ca6d3fa483ddc792c75b201b2a2048fc24cc87c5
emberfarkas <hubing91@163.com>
2016-10-27 19:54:29 +0800

    fix multthread

---
+++ Assets/Bacon/MyEventCmd.cs
@@ -0,0 +1,28 @@
+﻿using Maria;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bacon {
+    class MyEventCmd : EventCmd {
+
+        public static uint EVENT_PRESSUP    = 101;
+        public static uint EVENT_PRESSRIGHT = 102;
+        public static uint EVENT_PRESSDOWN  = 103;
+        public static uint EVENT_PRESSLEFT  = 104;
+
+        public static uint EVENT_SETUP_SCENE = 105;
+        public static uint EVENT_SETUP_VIEW = 106;
+        public static uint EVENT_SETUP_MAP = 107;
+
+        public static uint EVENT_STARTSCENE_ENTER = 108;
+
+        public static uint EVENT_ONBORN = 109;
+        public static uint EVENT_ONJOIN = 110;
+        public static uint EVENT_SETUP_UIROOT = 111;
+
+        public MyEventCmd(uint cmd) : base(cmd) {
+        }
+    }
+}
